fbffc1f34529fcbb9d16ee440257af27c55cb045
TrancaRazvan <razvantranca5@gmail.com>
2023-06-20 19:03:32 +0300

    commit 2

--- Teme/src/main/java/Week4_Assignment_II/Application.java
+++ Teme/src/main/java/Week4_Assignment_II/Application.java
@@ -1,11 +1,17 @@
 package Week4_Assignment_II;
 
+import javax.imageio.spi.RegisterableService;
+
 public class Application {
     public static void main(String[] args) {
     Iphone13 tel = new Iphone13();
     tel.addContact(1, 10239,"ofjaf","sadjias");
         tel.addContact(2, 1023339,"zzzz","qqqqqs");
         tel.addContact(3, 11139,"ffff","seeeees");
-        tel.lista();
+        tel.addContact(3, 11139,"f","s");
+        tel.listaContacte();
+        tel.getFirstContact();
+        tel.getLastContact();
+        tel.sendMessage(10239,"qwertyuiopa");
     }
 }
--- Teme/src/main/java/Week4_Assignment_II/Phone.java
+++ Teme/src/main/java/Week4_Assignment_II/Phone.java
@@ -1,6 +1,7 @@
 package Week4_Assignment_II;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class Phone {
     int numarLista;
@@ -8,17 +9,36 @@ public abstract class Phone {
     String nume;
     String prenume;
 
-    ArrayList<Integer> array = new ArrayList<>();
+    ArrayList<String> array = new ArrayList<>();
+    Iterator<String> iterator = array.iterator();
 
     void addContact(int numarLista, int numarTelefon, String nume, String prenume) {
         this.numarLista = numarLista;
         this.numarTelefon = numarTelefon;
         this.nume = nume;
         this.prenume = prenume;
-        array.add(numarLista);
+        array.add(nume + " " + prenume);
     }
-    void lista(){
+
+    void listaContacte() {
         System.out.println(array);
     }
 
+    public void getFirstContact() {
+        System.out.println(array.get(0));
+    }
+    public void getLastContact(){
+        System.out.println(array.get(array.size()-1));
+    }
+    public void sendMessage(int numarTelefon, String message){
+        this.numarTelefon = numarTelefon;
+        if(message.length() > 10){
+            System.out.println("Numarul maxim de caractere este 10!");
+        }else{
+        System.out.println(message);}
+    }
 }
+
+
+
+
